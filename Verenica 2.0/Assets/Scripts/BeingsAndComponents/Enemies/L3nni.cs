424dc27295eee5380eb39850693098bc1ce3c775
Daxtresaur <65520387+Daxtresaur@users.noreply.github.com>
2022-10-06 19:31:13 +0300

    0.1

---
+++ Verenica 2.0/Assets/Scripts/BeingsAndComponents/Enemies/L3nni.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L3nni : Enemy
+{
+    public override void Gimmick()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
